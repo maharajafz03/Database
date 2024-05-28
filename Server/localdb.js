0897d2f0f7d8229349b3c3d4dcd0aeb65d6cbce3
maharajafz03 <maharajafz1999@gmail.com>
2024-05-28 17:28:25 +0530

    ,.,

--- Server/localdb.js
+++ Server/localdb.js
@@ -5,7 +5,14 @@ async function main() {
     
     try {
         // Create a new user
-        const data = await User.create({ name: "nandy", age: 23, email: "babycorn43@dmail.eth", address:{street:"LA", city:"NYC"}});
+        const data = await User.create({ 
+            name: "nandy", 
+            age: 23, 
+            email: "babycorn43@dmail.eth", 
+            address:{
+                street:"LA",
+                city:"NYC"}
+            });
         // User.name = "nandy"
         console.log(data);
     } 
@@ -15,6 +22,12 @@ async function main() {
 }
 
 // Call the main function to execute the code
-main();
+// main();
+
+async function cool () {
+    const control = await User.findById({id: "6655b2c9e20ba485a8b52d1d"})
+    console.log(control)
+}
 
 
+cool()
