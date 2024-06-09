fcf9188e6c6545d1e0650ad2c080ec076aaaea7e
maharajafz03 <maharajafz1999@gmail.com>
2024-06-09 14:32:29 +0530

    dash

--- Server/router/paymentRouter.js
+++ Server/router/paymentRouter.js
@@ -1,1 +1,19 @@
 const router = router.express()
+
+
+app.get("/payment", (req, res) => {
+    res.send("your data is here")
+})
+
+app.post("/payment", (req, res) => {
+    res.send("your data is here")
+})
+
+app.put("/payment", (req, res) => {
+    res.send("your data is here")
+})
+
+app.delete("/payment", (req, res) => {
+    res.send("your data is here")
+})
+
